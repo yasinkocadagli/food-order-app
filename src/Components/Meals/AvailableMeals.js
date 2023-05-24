dbb25736f9a4cf6c1bdec346ad62bb7380435892
yasin <yasinkocadagli@gmail.com>
2023-05-24 22:13:44 +0300

    error handling

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -39,7 +39,7 @@ const AvailableMeals = () => {
     />
   ));
   return (
-    <section className={classes.meal}>
+    <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
       </Card>
